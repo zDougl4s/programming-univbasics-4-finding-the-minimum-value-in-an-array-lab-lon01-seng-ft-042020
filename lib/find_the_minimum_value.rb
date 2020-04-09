d4a85ff9a453c81c0278b34246d8c8a967530854
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-09 18:46:16 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,11 +1,10 @@
 def find_min_value(array)
 count = 11
-minimun_value = 1
 while count > array.length do
   if minimun_value == array[count]
     minimun_value = array[count]
   end
-  count -= 1
+  array.length -= 1
 end
 minimun_value
 end
