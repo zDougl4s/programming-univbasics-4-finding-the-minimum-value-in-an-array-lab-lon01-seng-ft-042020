e3b88d3cbe40b350c07c5c60f81240c30945af77
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-09 18:54:49 +0000

    Done.

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,11 +1,11 @@
 def find_min_value(array)
-count = 11
-minimun_value = 0
-while count > array.length do
-  if minimun_value == array[count]
+count = 0
+minimun_value = Float::INFINITY
+while count < array.length do
+  if minimun_value > array[count]
     minimun_value = array[count]
   end
-  count -= 1
+  count += 1
 end
 minimun_value
 end
